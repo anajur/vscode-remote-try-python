c8cee0c158060544f9a3c69a0c252cab9e16943a
Ana Júlia da Rosa <56845067+anajur@users.noreply.github.com>
2023-11-16 14:17:58 +0000

    Add funcionalidades

--- app.py
+++ app.py
@@ -1,33 +1,64 @@
 import random
 
-jogadas = ['pedra', 'papel', 'tesoura']
-jogador = input("Escreva sua jogada\n")
-computador = random.choice(['pedra', 'papel', 'tesoura'])
+pontuacao_jogador = 0
+pontuacao_computador = 0
 
-def jogarPedraPapelTesoura(jogador, computador):
-        if computador == jogador:
-            print('empate')
-        elif jogador == 'pedra' and computador == 'tesoura':
-            print('pedra vence')
-        elif jogador == 'pedra' and computador == 'papel':
-            print('papel vence')
-        elif jogador == 'tesoura' and computador == 'papel':
-            print('tesoura vence')
-        elif jogador == 'tesoura' and computador == 'pedra':
-            print('pedra vence')
-        elif jogador == 'papel' and computador == 'tesoura':
-            print('tesoura vence')
-        elif jogador == 'papel' and computador == 'pedra':
-            print('papel vence')
+def abrirTeclado():
+    jogador = input("Escreva sua jogada\n")
+    jogador = jogador.lower()
+    return jogador
+
+def jogarPedraPapelTesoura(jogador):
+    global pontuacao_jogador
+    global pontuacao_computador
+
+    computador = random.choice(['pedra', 'papel', 'tesoura'])
+
+    if computador == jogador:
+        print('empate')
+    elif jogador == 'pedra' and computador == 'tesoura':
+        pontuacao_jogador += 1
+        print('pedra vence')
+    elif jogador == 'pedra' and computador == 'papel':
+        pontuacao_computador += 1
+        print('papel vence')
+    elif jogador == 'tesoura' and computador == 'papel':
+        pontuacao_jogador += 1
+        print('tesoura vence')
+    elif jogador == 'tesoura' and computador == 'pedra':
+        pontuacao_computador += 1
+        print('pedra vence')
+    elif jogador == 'papel' and computador == 'tesoura':
+        pontuacao_computador += 1
+        print('tesoura vence')
+    elif jogador == 'papel' and computador == 'pedra':
+        pontuacao_jogador += 1
+        print('papel vence')
+    else:
+        print('jogada invalida')
+
+def exibirPontuacaoFinal():
+    print("Pontuação do jogador: ", pontuacao_jogador)
+    print("Pontuação do computador: ", pontuacao_computador)
+
+def jogarNovamente():
+    while True:
+        continuar = input("Deseja jogar novamente?\n")
+        continuar = continuar.lower()
+        if continuar == 'sim':
+            jogarPedraPapelTesoura(abrirTeclado())
         else:
-            print('jogada invalida')
+            exibirPontuacaoFinal()
+            print("Obrigado por jogar!")
+            break
 
-jogarPedraPapelTesoura(jogador, computador)
+jogarPedraPapelTesoura(abrirTeclado())
 
+jogarNovamente()
 
 from flask import Flask
 app = Flask(__name__)
-# write 'hello world' to the console
+
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
