75876e3d0625540eb1113fcc5016276a9b6c62a9
Ana Júlia da Rosa <56845067+anajur@users.noreply.github.com>
2023-11-16 03:53:20 +0000

    inicializando jogo

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import random
+
+jogadas = ['pedra', 'papel', 'tesoura']
+jogador = input("Escreva sua jogada\n")
+computador = random.choice(['pedra', 'papel', 'tesoura'])
+
+def jogarPedraPapelTesoura(jogador, computador):
+        if computador == jogador:
+            print('empate')
+        elif jogador == 'pedra' and computador == 'tesoura':
+            print('pedra vence')
+        elif jogador == 'pedra' and computador == 'papel':
+            print('papel vence')
+        elif jogador == 'tesoura' and computador == 'papel':
+            print('tesoura vence')
+        elif jogador == 'tesoura' and computador == 'pedra':
+            print('pedra vence')
+        elif jogador == 'papel' and computador == 'tesoura':
+            print('tesoura vence')
+        elif jogador == 'papel' and computador == 'pedra':
+            print('papel vence')
+        else:
+            print('jogada invalida')
+
+jogarPedraPapelTesoura(jogador, computador)
+
 
 from flask import Flask
 app = Flask(__name__)
-
+# write 'hello world' to the console
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
